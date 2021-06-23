c245d689a0b094bbf91d3539dcb7d81d3dfb6156
vasireddy ganesh <51131045+ganeshvasi@users.noreply.github.com>
2021-06-23 18:10:00 +0530

    Delete c_practice directory

--- c_practice/10.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    for(int i=5;i>2;i--)
-    printf("%s \n","hello ");//single line conditional or loop statements dont need braces
-    printf("go carono \n");
-    return 0;
-}
--- c_practice/18.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
- puts("hello world");//even \n is not written in puts ..it always ends with \n..this is the difference btw printf and puts
- prints("how r u ra");
- return 0;
-}
